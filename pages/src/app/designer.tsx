8094588da69ff25ab387cfb29e6679b0d57be8f7
leon-js <42924541+leon-js@users.noreply.github.com>
2023-09-19 21:44:13 +0800

    feat:升级组件库版本

--- pages/src/app/designer.tsx
+++ pages/src/app/designer.tsx
@@ -236,7 +236,7 @@ function spaDesignerConfig ({ appData, designerRef, context }) {
         } else if (isH5) {
           resolve(['https://ali-ec.static.yximgs.com/kos/nlav11092/test/091412/lib/libEdt.js'])
         } else {
-          resolve(['https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.90/2023-09-12_17-33-07/edit.js', 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.64/2023-09-11_17-28-40/edit.js'])
+          resolve(['https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.3.2/2023-09-19_21-39-53/edit.js', 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.64/2023-09-11_17-28-40/edit.js'])
         }
       })
     },
