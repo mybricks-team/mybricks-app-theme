2bbaa81838dbcaa0ef3b367c1f09ffe9d135be8b
leon-js <42924541+leon-js@users.noreply.github.com>
2025-11-24 11:22:48 +0800

    feat:老数据兼容

--- pages/src/pages/design/editors/Design/themeToken/init/initTheme.ts
+++ pages/src/pages/design/editors/Design/themeToken/init/initTheme.ts
@@ -7,6 +7,8 @@ export const initTheme = (defaultTheme?: ThemeData) => {
     return DEFAULT_THEME
   }
 
+  compatible1(defaultTheme);
+
   // 数据兼容
   defaultTheme.variables.forEach((variable) => {
     variable.title = variable.key
@@ -27,4 +29,66 @@ export const initTheme = (defaultTheme?: ThemeData) => {
   })
 
   return defaultTheme
+}
+
+const compatible1 = (defaultTheme) => {
+  // 最初的版本
+  if (defaultTheme.variables[0].id !== "mybricks@theme") {
+    return;
+  }
+
+  console.log("[数据兼容 - compatible1]", JSON.parse(JSON.stringify({
+    defaultTheme,
+    DEFAULT_THEME
+  })))
+
+  // const cssVarMap = {
+  //   "--mybricks-primary-color": "--mb-color-primary",
+  //   "--mybricks-primary-color-hover": "--mb-color-primary-hover",
+  //   "--mybricks-primary-color-active": "--mb-color-primary-active",
+  //   "--mybricks-success-color": "--mb-color-success",
+  //   "--mybricks-success-color-hover": "--mb-color-success-hover",
+  //   "--mybricks-success-color-active": "--mb-color-success-active",
+  //   "--mybricks-error-color": "--mb-color-error",
+  //   "--mybricks-error-color-hover": "--mb-color-error-hover",
+  //   "--mybricks-error-color-active": "--mb-color-error-active",
+  //   "--mybricks-warning-color": "--mb-color-warning",
+  //   "--mybricks-warning-color-hover": "--mb-color-warning-hover",
+  //   "--mybricks-warning-color-active": "--mb-color-warning-active",
+  //   "--mybricks-info-color": "--mb-color-info"
+  // }
+
+  const cssVarMap = {
+    "--mb-color-primary": "--mybricks-primary-color",
+    "--mb-color-primary-hover": "--mybricks-primary-color-hover",
+    "--mb-color-primary-active": "--mybricks-primary-color-active",
+    "--mb-color-success": "--mybricks-success-color",
+    "--mb-color-success-hover": "--mybricks-success-color-hover",
+    "--mb-color-success-active": "--mybricks-success-color-active",
+    "--mb-color-error": "--mybricks-error-color",
+    "--mb-color-error-hover": "--mybricks-error-color-hover",
+    "--mb-color-error-active": "--mybricks-error-color-active",
+    "--mb-color-warning": "--mybricks-warning-color",
+    "--mb-color-warning-hover": "--mybricks-warning-color-hover",
+    "--mb-color-warning-active": "--mybricks-warning-color-active",
+    "--mb-color-info": "--mybricks-info-color"
+  }
+
+  const mybricksTheme = defaultTheme.variables[0]
+  const customTheme = defaultTheme.variables[2]
+
+  const mybricksThemeColorMap = mybricksTheme.configs.reduce((pre, cur) => {
+    pre[cur.key] = cur.value
+    return pre;
+  }, {})
+
+  defaultTheme.variables = DEFAULT_THEME.variables;
+  defaultTheme.variables[0].variables[1].configs.push(...customTheme.configs);
+
+  defaultTheme.variables[0].variables[0].configs.forEach((config) => {
+    const key = cssVarMap[config.key]
+    if (key) {
+      config.value = mybricksThemeColorMap[key]
+    }
+  })
 }
